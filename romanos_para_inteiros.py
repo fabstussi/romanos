8de63df6670587993a62124b9acc7cd781de690d
Fabiano Stussi Pereria <fabstussi@outlook.com>
2020-08-29 22:29:38 -0300

    Ultimos acertos.

--- romanos_para_inteiros.py
+++ romanos_para_inteiros.py
@@ -1,7 +1,8 @@
 def romano_para_inteiro(s: str) -> int:
     s = s.upper()
     romanos = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if s not in romanos:
+    for verificador in s:
+    if s[0] not in romanos:
         return 0
     resp = 0
     for index, valor in enumerate(s[:len(s) - 1]):
