121cf8311a462a85eb6e9207b62ea971505930e4
Fabiano Stussi Pereria <fabstussi@outlook.com>
2020-08-31 11:32:45 -0300

    Adicionada uma breve explicação.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from calculo_romano import Calculos
 
+print('''Olá, esse é o Calculo romano.
+Aqui você consegue fazer calculos das operações fundamentais.
+basta digitar a operação desejada completa.
+Ex.: V + IV ou XV - IX ou X * II ou CXX / IV''')
+
 resultado = Calculos(input('Digite um calculo com números romanos: '))
 print(resultado.fazer_calculo())
